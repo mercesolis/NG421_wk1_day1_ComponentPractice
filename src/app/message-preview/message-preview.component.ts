c0cbdb2403d3d87b86b58cec7f42fa091721e05b
mercesolis <mersolis@live.com>
2020-08-17 12:18:06 -0500

    messages work!

--- src/app/message-preview/message-preview.component.ts
+++ src/app/message-preview/message-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import messages from '../messages';
 
 @Component({
   selector: 'app-message-preview',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagePreviewComponent implements OnInit {
 
+  messages = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.messages = messages;
   }
 
 }
