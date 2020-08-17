02b65bda1392724c63c92cd977ad57ac9a21552b
mercesolis <mersolis@live.com>
2020-08-17 11:33:50 -0500

    add message preview component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DateTimeComponent } from './date-time/date-time.component';
 import { TransactionsPanelComponent } from './transactions-panel/transactions-panel.component';
+import { MessagePreviewComponent } from './message-preview/message-preview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DateTimeComponent,
-    TransactionsPanelComponent
+    TransactionsPanelComponent,
+    MessagePreviewComponent
   ],
   imports: [
     BrowserModule
