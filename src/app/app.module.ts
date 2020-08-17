f63621ca3e00fdd9e25198f32cb958dc41b83c92
mercesolis <mersolis@live.com>
2020-08-17 13:01:16 -0500

    add all other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { TransactionsPanelComponent } from './transactions-panel/transactions-pa
 import { MessagePreviewComponent } from './message-preview/message-preview.component';
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { CommentsComponent } from './comments/comments.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { OrdersComponent } from './orders/orders.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { AreaChartComponent } from './area-chart/area-chart.component';
+import { DonutChartComponent } from './donut-chart/donut-chart.component';
+import { TasksPanelComponent } from './tasks-panel/tasks-panel.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +24,16 @@ import { TopNavComponent } from './top-nav/top-nav.component';
     TransactionsPanelComponent,
     MessagePreviewComponent,
     TaskItemComponent,
-    TopNavComponent
+    TopNavComponent,
+    SideNavComponent,
+    CommentsComponent,
+    TasksComponent,
+    OrdersComponent,
+    TicketsComponent,
+    AreaChartComponent,
+    DonutChartComponent,
+    TasksPanelComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
